1799085a091bfaad05e1df0d690edf00e11b7143
Emily Feng <efeng2@uw.edu>
2025-03-29 15:15:21 +0800

    fixed profile res

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -62,7 +62,7 @@ export function Header(props) {
                         </div>
                     </div>
                     <div className="col-md-4 text-center mb-4 mt-5 mt-md-4">
-                    <img src="https://efeng2.github.io/page/images/profile.jpg" alt="Profile" className="img-fluid rounded-circle" />
+                    <img src="https://efeng2.github.io/page/images/profile.png" alt="Profile" className="img-fluid rounded-circle" />
                     </div>
                 </div>
             </div>
