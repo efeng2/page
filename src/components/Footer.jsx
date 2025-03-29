782e7443cc72d00808e9f00d9ba16eae6037a811
Emily Feng <efeng2@uw.edu>
2025-03-29 13:54:40 +0800

    fixed ajax

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,12 +12,11 @@ export function Footer(props) {
                         <a href="https://www.linkedin.com/in/emily-feng-b37b1223a/" target="_blank" className="nav-link hover-orange"><i className="bi bi-linkedin m-1"></i>LinkedIn</a>
                     </li>
                     <li className="nav-item">
-                        <a href="https://blog.csdn.net/2401_86205687?spm=1000.2115.3001.5343" target="_blank" className="nav-link hover-orange"><i className="bi bi-csdn m-1"></i>CSDN</a>
-                    </li>
-                    <li className="nav-item">
                         <a href="https://dev.to/efeng2" target="_blank" className="nav-link hover-orange"><i className="bi bi-dev m-1"></i>Dev</a>
                     </li>
-
+                    <li className="nav-item">
+                        <a href="https://blog.csdn.net/2401_86205687?spm=1000.2115.3001.5343" target="_blank" className="nav-link hover-orange"><i className="bi bi-csdn m-1"></i>CSDN</a>
+                    </li>
                     <li>
                         <a href="https://space.bilibili.com/3493258667755815?spm_id_from=333.1007.0.0" target="_blank" className="nav-link hover-orange"><i className="bi bi-bilibili m-1"></i>Bilibili</a>
                     </li>
